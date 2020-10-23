bc861de0c6b08a801b3b94ed51796db7a8dc77d3
Willian Tessaro Lunardi <williantessarolunardi@gmail.com>
2020-10-24 00:53:53 +0200

    Add figure

--- q-learning.py
+++ q-learning.py
@@ -2,15 +2,16 @@ import env.gridseedpygame as env
 import numpy as np
 import time
 
-# The game support multiple seeds and players. In this example, I am considering only one player, 4 rows and 4 cols, and one seed
+'''
+The game support multiple seeds and players. In this example, I am considering only one player, 
+4 rows and 4 cols, and one seed 
+'''
 
-# Number of states = n * n - 1, where n are the number of cells in the grid
-n_states = (env.gm.ROW_COUNT * env.gm.COLUMN_COUNT) * ((env.gm.ROW_COUNT * env.gm.COLUMN_COUNT) - env.gm.NUMB_SEEDS)
-n_actions = 4
-os_size = [env.gm.ROW_COUNT, env.gm.COLUMN_COUNT, env.gm.ROW_COUNT, env.gm.COLUMN_COUNT, 4]
+# q-table size
+q_size = [env.gm.ROW_COUNT, env.gm.COLUMN_COUNT, env.gm.ROW_COUNT, env.gm.COLUMN_COUNT, 4]
+q_table = np.random.uniform(low=0, high=0, size=q_size)
 
 # Q-learning parameters
-q_table = np.random.uniform(low=0, high=0, size=os_size)
 learning_rate = 0.1
 discount = 0.95
 episodes = 2000000
@@ -18,24 +19,14 @@ zero_eps_at = int(0.8 * episodes)
 
 
 def get_state(state):
-    # print(state)
-    # Used to retrieval the of the states where seed is at seed_pos[0] and seed_pos[1]
-    seed_pos = env.game.reset().seed_pos
-    # Used to retrieval of the actions' reward
-    one_pos = env.game.reset().one_pos
+    seed_pos = state.seed_pos
+    one_pos = state.one_pos
     return tuple([seed_pos[0], seed_pos[1], one_pos[0], one_pos[1]])
 
 
 if __name__ == '__main__':
-    print(f"Number of possible states: {n_states}")
-
-    state = env.game.reset()
-    matrix_state = get_state(state)
-
-    print(matrix_state)
-    print(q_table[matrix_state])
-    print(np.argmax(q_table[matrix_state]))
-    print(q_table[matrix_state][np.argmax(q_table[matrix_state])])
+    # Initial state
+    matrix_state = get_state(env.game.reset())
 
     cur_it = 0
     while cur_it < episodes:
@@ -50,7 +41,7 @@ if __name__ == '__main__':
             action = np.random.randint(0, 4)
 
         # Update game with action, player one
-        new_state, reward = env.game.step(action + 1, 1)
+        new_state, reward = env.game.step(action + 1, 1)    # reward 0 if got seed, -1 otherwise
         new_matrix_state = get_state(new_state)
 
         # Q-learning equation
